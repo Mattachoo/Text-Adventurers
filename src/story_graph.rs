6cf5b0fc0a8ddbc403514c233b0de9a817cefa1c
Andrew McLees <andrewmclees@google.com>
2020-04-24 22:26:31 -0500

    Implement the run fn for StoryChoice elements

--- src/story_graph.rs
+++ src/story_graph.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use crate::choice::Choice;
 use crate::io::Interface;
 use crate::world::World;
 
@@ -81,14 +82,30 @@ impl StoryChoice {
         interface: &mut I,
         world: World,
     ) {
-        // TODO(amclees): Implement this function.
+        let chosen = interface.choose(self.options.clone());
+        if let Some(result_text) = chosen.result_text {
+            interface.write(result_text.as_str());
+        }
+        if let Some(next_node) = chosen.next_node {
+            let next_node = graph.get_node(next_node.as_str());
+            next_node.elements[0].run(0, next_node, graph, interface, world);
+        } else {
+            current_node.elements[index + 1].run(index + 1, current_node, graph, interface, world);
+        }
     }
 }
 
+#[derive(Clone)]
 pub struct StoryOption {
     intro_text: String,
     result_text: Option<String>,
     next_node: Option<String>,
+}
+
+impl Choice for StoryOption {
+    fn describe(&self) -> String {
+        self.intro_text.clone()
+    }
 }
 
 pub struct StoryText {
@@ -114,9 +131,7 @@ mod tests {
     use std::collections::VecDeque;
 
     use super::*;
-    use crate::character::Character;
     use crate::io::TestInterface;
-    use crate::stat::StatBlock;
     use crate::world::World;
 
     #[test]
@@ -139,15 +154,96 @@ mod tests {
         );
         let graph = StoryGraph::new();
         let mut interface = TestInterface::new(VecDeque::new());
-        let world = World {
-            player: Character {
-                stats: StatBlock::new(),
-            },
-        };
+        let world = World::empty();
         node.elements[0].run(0, &node, &graph, &mut interface, world);
         assert_eq!(
             interface.written,
             "Sample text.\nGoodbye! Thanks for playing."
+        );
+    }
+
+    #[test]
+    pub fn choices_without_a_next_node_run() {
+        let node = StoryNode::new(
+            "FooNode".to_string(),
+            vec![
+                StoryElement::Choice(StoryChoice {
+                    options: vec![
+                        StoryOption {
+                            intro_text: "Foo\n".to_string(),
+                            result_text: None,
+                            next_node: None,
+                        },
+                        StoryOption {
+                            intro_text: "Bar\n".to_string(),
+                            result_text: Some("Baz\n".to_string()),
+                            next_node: None,
+                        },
+                    ],
+                }),
+                StoryElement::Exit,
+            ],
+        );
+        let graph = StoryGraph::new();
+        let mut interface = TestInterface::new(VecDeque::from(vec![1, 0]));
+        let world = World::empty();
+        node.elements[0].run(0, &node, &graph, &mut interface, world);
+        assert_eq!(interface.written, "Baz\nGoodbye! Thanks for playing.");
+        interface.written.clear();
+        let world = World::empty();
+        node.elements[0].run(0, &node, &graph, &mut interface, world);
+        assert_eq!(interface.written, "Goodbye! Thanks for playing.");
+    }
+
+    #[test]
+    pub fn choices_with_a_next_node_run() {
+        let mut graph = StoryGraph::new();
+        graph.add_node(StoryNode::new(
+            "FooNode".to_string(),
+            vec![
+                StoryElement::Text(StoryText {
+                    text: "Choose Foo or Bar\n".to_string(),
+                }),
+                StoryElement::Choice(StoryChoice {
+                    options: vec![
+                        StoryOption {
+                            intro_text: "Foo\n".to_string(),
+                            result_text: Some("Chose Foo\n".to_string()),
+                            next_node: Some("FooNode".to_string()),
+                        },
+                        StoryOption {
+                            intro_text: "Bar\n".to_string(),
+                            result_text: None,
+                            next_node: Some("BarNode".to_string()),
+                        },
+                    ],
+                }),
+            ],
+        ));
+        graph.add_node(StoryNode::new(
+            "BarNode".to_string(),
+            vec![
+                StoryElement::Text(StoryText {
+                    text: "Reached Bar\n".to_string(),
+                }),
+                StoryElement::Exit,
+            ],
+        ));
+        let mut interface = TestInterface::new(VecDeque::from(vec![0, 0, 0, 1]));
+        let world = World::empty();
+        let node = graph.get_node("FooNode");
+        node.elements[0].run(0, node, &graph, &mut interface, world);
+        assert_eq!(
+            interface.written,
+            "Choose Foo or Bar
+Chose Foo
+Choose Foo or Bar
+Chose Foo
+Choose Foo or Bar
+Chose Foo
+Choose Foo or Bar
+Reached Bar
+Goodbye! Thanks for playing."
         );
     }
 }
