736d56647ee9db5cf29de4839e94580684b2d324
Andrew McLees <andrewmclees@google.com>
2020-04-24 21:41:54 -0500

    Implement the run function for StoryText elements

--- src/story_graph.rs
+++ src/story_graph.rs
@@ -35,26 +35,34 @@ impl StoryNode {
             elements: Vec::new(),
         }
     }
+
+    pub fn new(name: String, elements: Vec<StoryElement>) -> StoryNode {
+        StoryNode { name, elements }
+    }
 }
 
-enum StoryElement {
+pub enum StoryElement {
     Text(StoryText),
     Choice(StoryChoice),
+    Exit,
 }
 
 impl StoryElement {
     fn run<I: Interface>(
         &self,
-        index: i64,
+        index: usize,
         current_node: &StoryNode,
         graph: &StoryGraph,
-        interface: I,
+        interface: &mut I,
         world: World,
     ) {
         match self {
             StoryElement::Text(text) => text.run(index, current_node, graph, interface, world),
             StoryElement::Choice(choice) => {
                 choice.run(index, current_node, graph, interface, world)
+            }
+            StoryElement::Exit => {
+                interface.write("Goodbye! Thanks for playing.");
             }
         }
     }
@@ -67,10 +75,10 @@ pub struct StoryChoice {
 impl StoryChoice {
     fn run<I: Interface>(
         &self,
-        index: i64,
+        index: usize,
         current_node: &StoryNode,
         graph: &StoryGraph,
-        interface: I,
+        interface: &mut I,
         world: World,
     ) {
         // TODO(amclees): Implement this function.
@@ -90,24 +98,56 @@ pub struct StoryText {
 impl StoryText {
     fn run<I: Interface>(
         &self,
-        index: i64,
+        index: usize,
         current_node: &StoryNode,
         graph: &StoryGraph,
-        interface: I,
+        interface: &mut I,
         world: World,
     ) {
-        // TODO(amclees): Implement this function.
+        interface.write(self.text.as_str());
+        current_node.elements[index + 1].run(index, current_node, graph, interface, world);
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::VecDeque;
+
     use super::*;
+    use crate::character::Character;
+    use crate::io::TestInterface;
+    use crate::stat::StatBlock;
+    use crate::world::World;
 
     #[test]
     pub fn stores_nodes() {
         let mut graph = StoryGraph::new();
         graph.add_node(StoryNode::empty_node(String::from("FooNode")));
         assert_eq!(graph.get_node("FooNode").name, "FooNode");
+    }
+
+    #[test]
+    pub fn text_nodes_write_text() {
+        let node = StoryNode::new(
+            "FooNode".to_string(),
+            vec![
+                StoryElement::Text(StoryText {
+                    text: "Sample text.\n".to_string(),
+                }),
+                StoryElement::Exit,
+            ],
+        );
+        let graph = StoryGraph::new();
+        let mut interface = TestInterface::new(VecDeque::new());
+        let world = World {
+            player: Character {
+                stats: StatBlock::new(),
+            },
+        };
+        node.elements[0].run(0, &node, &graph, &mut interface, world);
+        assert_eq!(
+            interface.written,
+            "Sample text.\nGoodbye! Thanks for playing."
+        );
     }
 }
